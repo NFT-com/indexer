9b9df5f0dd8c1402ecd0e19c4e4cb88ed73b517e
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-01-19 15:50:40 +0000

    added more events, fix some bugs on the subscriber and sources

--- events/events.go
+++ events/events.go
@@ -3,109 +3,70 @@ package events
 import "github.com/ethereum/go-ethereum/common"
 
 type Event interface {
-	ID() string
-	Chain() string
-	Network() string
-	Topic() string
+	String() string
 }
 
 // FIXME: Clean up code below.
 
-type OrdersMatched struct {
-	id       string         // Hash(block hash + transaction hash + log index)
-	chain    string         // Ethereum
-	network  string         // Mainnet
-	topic    string         // Transfer
-	Address  common.Address // Event Contract Address
-	BuyHash  common.Hash
-	SellHash common.Hash
-	Maker    common.Address
-	Taker    common.Address
-	Price    uint64
-	Metadata common.Hash
-}
-
-func NewOrdersMatched(
-	id, chain, network, topic string,
-	address common.Address,
-	buyHash, sellHash common.Hash,
-	maker, taker common.Address,
-	price uint64,
-	metadata common.Hash,
-) Event {
-	return &OrdersMatched{
-		id:       id,
-		chain:    chain,
-		network:  network,
-		topic:    topic,
-		Address:  address,
-		BuyHash:  buyHash,
-		SellHash: sellHash,
-		Maker:    maker,
-		Taker:    taker,
-		Price:    price,
-		Metadata: metadata,
-	}
-}
-
-func (t *OrdersMatched) ID() string {
-	return t.id
-}
-
-func (t *OrdersMatched) Chain() string {
-	return t.chain
-}
-
-func (t *OrdersMatched) Network() string {
-	return t.network
-}
-
-func (t *OrdersMatched) Topic() string {
-	return t.topic
-}
-
 type Transfer struct {
-	id      string         // Hash(block hash + transaction hash + log index)
-	chain   string         // Ethereum
-	network string         // Mainnet
-	topic   string         // Transfer
+	ID      string         // Hash(block hash + transaction hash + log index)
+	Chain   string         // Ethereum
+	Network string         // Mainnet
+	Topic   string         // Transfer
 	Address common.Address // Event Contract Address
 	From    common.Address // From address
 	To      common.Address // To address
-	NftID   uint64         // IF of the NFT
+	NftID   uint64         // ID of the NFT
 }
 
-func NewTransfer(
-	id, chain, network, topic string,
-	address, from, to common.Address,
-	nftID uint64,
-) Event {
-	t := Transfer{
-		id:      id,
-		chain:   chain,
-		network: network,
-		topic:   topic,
-		Address: address,
-		From:    from,
-		To:      to,
-		NftID:   nftID,
-	}
-
-	return &t
+func (t *Transfer) String() string {
+	return t.ID
 }
 
-func (t *Transfer) ID() string {
-	return t.id
+type TransferSingle struct {
+	ID       string         // Hash(block hash + transaction hash + log index)
+	Chain    string         // Ethereum
+	Network  string         // Mainnet
+	Topic    string         // Transfer
+	Address  common.Address // Event Contract Address
+	Operator common.Address // The one who started this transaction.
+	From     common.Address // From address
+	To       common.Address // To address
+	NftID    uint64         // ID of the NFT
+	Value    uint64         // ID of the NFT
 }
 
-func (t *Transfer) Chain() string {
-	return t.chain
+func (t *TransferSingle) String() string {
+	return t.ID
 }
 
-func (t *Transfer) Network() string {
-	return t.network
+type TransferBatch struct {
+	ID       string         // Hash(block hash + transaction hash + log index)
+	Chain    string         // Ethereum
+	Network  string         // Mainnet
+	Topic    string         // Transfer
+	Address  common.Address // Event Contract Address
+	Operator common.Address // The one who started this transaction.
+	From     common.Address // From address
+	To       common.Address // To address
+	NftIDs   []uint64       // ID of the NFT
+	Values   []uint64       // ID of the NFT
 }
 
-func (t *Transfer) Topic() string {
-	return t.topic
+func (t *TransferBatch) String() string {
+	return t.ID
+}
+
+type URI struct {
+	ID      string         // Hash(block hash + transaction hash + log index)
+	Chain   string         // Ethereum
+	Network string         // Mainnet
+	Topic   string         // Transfer
+	Address common.Address // Event Contract Address
+	NftID   uint64         // ID of the NFT
+	URI     string         // New token URI
+}
+
+func (t *URI) String() string {
+	return t.ID
 }
