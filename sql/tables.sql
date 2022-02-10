236c1a6bef4472a4483c0251cf9a1677064d050a
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-02-10 11:19:12 +0000

    updated tables with comments

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,44 +1,58 @@
 -- Creation of chain table
 CREATE TABLE IF NOT EXISTS chain (
-    id BIGSERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(64) NOT NULL,
     description TEXT NOT NULL,
     symbol VARCHAR(16) NOT NULL,
     network VARCHAR(64) NOT NULL,
     chain_id INT NOT NULL
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 -- Creation of collection table
 CREATE TABLE IF NOT EXISTS collection (
-    id BIGSERIAL PRIMARY KEY,
-    network_id BIGINT NOT NULL, -- FOREIGN
-    name VARCHAR(64) NOT NULL,
+    id UUID PRIMARY KEY,
+    chain_id UUID NOT NULL, -- FOREIGN
+    name TEXT NOT NULL,
     description TEXT NOT NULL,
     symbol VARCHAR(5) NOT NULL,
     address VARCHAR(66) NOT NULL,
     abi TEXT NOT NULL,
     standard VARCHAR(16) NOT NULL
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 -- Creation of event table
 CREATE TABLE IF NOT EXISTS event (
     id VARCHAR(66) PRIMARY KEY,
-    network_id BIGINT NOT NULL, -- FOREIGN
-    collection_id BIGINT NOT NULL, -- FOREIGN
+    chain_id UUID NOT NULL, -- FOREIGN
+    collection_id UUID NOT NULL, -- FOREIGN
     block BIGINT NOT NULL,
     transaction_hash VARCHAR(66) NOT NULL,
     type VARCHAR(64) NOT NULL,
     data JSONB
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 -- Creation of nft table
 CREATE TABLE IF NOT EXISTS nft (
-    id BIGSERIAL PRIMARY KEY,
-    collection_id BIGINT NOT NULL, -- FOREIGN
-    nft_collection_id INT NOT NULL, -- ID of the nft in the collection
-    name VARCHAR(64) NOT NULL,
+    id UUID PRIMARY KEY,
+    collection_id UUID NOT NULL, -- FOREIGN
+    token_id UUID NOT NULL, -- ID of the nft in the collection
+    owner VARCHAR(66) NOT NULL,
+    name TEXT NOT NULL,
     uri TEXT NOT NULL,
-    data JSONB
+    rarity INT NOT NULL,
+    data JSONB,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 -- FIXME: Should we use FOREIGN KEY?
