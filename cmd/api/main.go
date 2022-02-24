317afafc519376626386cb62ec81fd71539659e3
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-02-24 17:53:46 +0000

    started boilerplate of the api

---
+++ cmd/api/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/ziflex/lecho/v2"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	"github.com/spf13/pflag"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("failure: %v\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func run() error {
+	// Signal catching for clean shutdown.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+
+	// Command line parameter initialization.
+	var (
+		flagPort     string
+		flagLogLevel string
+	)
+
+	pflag.StringVarP(&flagPort, "port", "p", "8081", "server port")
+	pflag.StringVarP(&flagLogLevel, "log-level", "l", "info", "log level")
+	pflag.Parse()
+
+	// Logger initialization.
+	zerolog.TimestampFunc = func() time.Time { return time.Now().UTC() }
+	log := zerolog.New(os.Stderr).With().Timestamp().Logger().Level(zerolog.DebugLevel)
+	level, err := zerolog.ParseLevel(flagLogLevel)
+	if err != nil {
+		return err
+	}
+	log = log.Level(level)
+	eLog := lecho.From(log)
+
+	server := echo.New()
+	server.HideBanner = true
+	server.HidePort = true
+	server.Logger = eLog
+	server.Use(lecho.Middleware(lecho.Config{Logger: eLog}))
+
+	failed := make(chan error)
+	done := make(chan struct{})
+
+	go func() {
+		log.Info().Msg("Launching subscriber")
+
+		log.Info().Msg("Stopped subscriber")
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case <-sig:
+
+	case err := <-failed:
+		return err
+	}
+
+	go func() {
+		<-sig
+		log.Fatal().Msg("forced interruption")
+	}()
+
+	<-done
+
+	go func() {
+		log.Info().Msg("Dispatcher Server starting")
+
+		err := server.Start(fmt.Sprint(":", flagPort))
+
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Warn().Err(err).Msg("Dispatcher Server failed")
+			failed <- err
+			return
+		}
+
+		log.Info().Msg("Dispatcher Server stopped")
+		close(done)
+	}()
+
+	select {
+	case <-sig:
+		log.Info().Msg("Dispatcher Server stopping")
+	case <-done:
+		log.Info().Msg("Dispatcher Server done")
+	case err := <-failed:
+		log.Warn().Msg("Dispatcher Server aborted")
+		return err
+	}
+	go func() {
+		<-sig
+		log.Warn().Msg("forcing exit")
+		os.Exit(1)
+	}()
+
+	// The following code starts a shutdown with a certain timeout and makes
+	// sure that the main executing components are shutting down within the
+	// allocated shutdown time. Otherwise, we will force the shutdown and log
+	// an error. We then wait for shutdown on each component to complete.
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = server.Shutdown(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("could not shut down dispatcher API")
+		return err
+	}
+
+	return nil
+}
