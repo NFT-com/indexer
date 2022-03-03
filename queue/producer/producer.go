03fa3c0e623483934686e0eb0b5e26829a1129e2
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-03-03 17:57:44 +0000

    main logic for the watcher with websockets

---
+++ queue/producer/producer.go
@@ -0,0 +1,63 @@
+package producer
+
+import (
+	"encoding/json"
+
+	"github.com/adjust/rmq/v4"
+
+	"github.com/NFT-com/indexer/job"
+)
+
+type Producer struct {
+	connection     rmq.Connection
+	discoveryQueue string
+	parsingQueue   string
+}
+
+func NewProducer(connection rmq.Connection, discoveryQueue string, parsingQueue string) (*Producer, error) {
+	p := Producer{
+		connection:     connection,
+		discoveryQueue: discoveryQueue,
+		parsingQueue:   parsingQueue,
+	}
+
+	return &p, nil
+}
+
+func (p *Producer) PublishDiscoveryJob(discoveryJob job.Discovery) error {
+	q, err := p.connection.OpenQueue(p.discoveryQueue)
+	if err != nil {
+		return err
+	}
+
+	jobPayload, err := json.Marshal(discoveryJob)
+	if err != nil {
+		return err
+	}
+
+	err = q.PublishBytes(jobPayload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Producer) PublishParsingJob(parsingJob job.Parsing) error {
+	q, err := p.connection.OpenQueue(p.parsingQueue)
+	if err != nil {
+		return err
+	}
+
+	jobPayload, err := json.Marshal(parsingJob)
+	if err != nil {
+		return err
+	}
+
+	err = q.PublishBytes(jobPayload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
