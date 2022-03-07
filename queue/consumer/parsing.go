b24790865d1f2e758a6536061ae8480241d306ef
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-03-07 15:35:37 +0000

    added dispatcher

--- queue/consumer/parsing.go
+++ queue/consumer/parsing.go
@@ -3,16 +3,23 @@ package consumer
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/NFT-com/indexer/function"
+	"github.com/rs/zerolog"
 
 	"github.com/NFT-com/indexer/job"
 	"github.com/adjust/rmq/v4"
 )
 
 type Parsing struct {
+	log        zerolog.Logger
+	dispatcher function.Dispatcher
 }
 
-func NewParsingConsumer() (*Parsing, error) {
-	c := Parsing{}
+func NewParsingConsumer(log zerolog.Logger, dispatcher function.Dispatcher) (*Parsing, error) {
+	c := Parsing{
+		log:        log,
+		dispatcher: dispatcher,
+	}
 
 	return &c, nil
 }
@@ -23,21 +30,33 @@ func (d *Parsing) Consume(delivery rmq.Delivery) {
 	payload := []byte(delivery.Payload())
 	parsingJob := job.Parsing{}
 
+	return
+
 	err := json.Unmarshal(payload, &parsingJob)
 	if err != nil {
-		fmt.Println(1, err)
-		// FIXME: Logg
-		err = delivery.Reject()
-		if err != nil {
-			fmt.Println(2, err)
-			// FIXME: LOG
+		if rejectErr := delivery.Reject(); rejectErr != nil {
+			d.log.Error().Err(err).AnErr("reject_error", rejectErr).Msg("failed to unmarshal message")
+			return
 		}
+
+		d.log.Error().Err(err).Msg("failed to unmarshal message")
+		return
+	}
+
+	err = d.dispatcher.Dispatch("test", payload)
+	if err != nil {
+		if rejectErr := delivery.Reject(); rejectErr != nil {
+			d.log.Error().Err(err).AnErr("reject_error", rejectErr).Msg("failed to dispatch message")
+			return
+		}
+
+		d.log.Error().Err(err).Msg("failed to dispatch message")
 		return
 	}
 
 	err = delivery.Ack()
 	if err != nil {
-		fmt.Println(5, err)
-		// FIXME: Logg
+		d.log.Error().Err(err).Msg("failed to acknowledge message")
+		return
 	}
 }
