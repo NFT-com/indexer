db70b70df508f9cca18092f243a8d259126e1a6f
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-03-15 10:50:01 +0000

    updated with pr comments

--- jobs/discovery.go
+++ jobs/discovery.go
@@ -1,7 +1,7 @@
 package jobs
 
 type Discovery struct {
-	ID           ID       `json:"id"`
+	ID           string   `json:"id"`
 	ChainURL     string   `json:"chain_url"`
 	ChainType    string   `json:"chain_type"`
 	BlockNumber  string   `json:"block_number"`
