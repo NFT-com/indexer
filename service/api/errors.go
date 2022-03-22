49fbdbc652dc92e8cc08201602c148a5b986fa72
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-03-22 10:42:43 +0000

    updated returning errors

--- service/api/errors.go
+++ service/api/errors.go
@@ -7,17 +7,17 @@ import (
 )
 
 func unpackError(err error) *echo.HTTPError {
-	return echo.NewHTTPError(http.StatusBadRequest, err)
+	return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 }
 
 func validateError(err error) *echo.HTTPError {
-	return echo.NewHTTPError(http.StatusBadRequest, err)
+	return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 }
 
 func parsingError(err error) *echo.HTTPError {
-	return echo.NewHTTPError(http.StatusBadRequest, err)
+	return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 }
 
 func apiError(err error) *echo.HTTPError {
-	return echo.NewHTTPError(http.StatusInternalServerError, err)
+	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 }
