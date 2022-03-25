c28aad3b74d5ec1c7f686eb796c91602df506fa3
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-03-25 16:07:57 +0000

    added marketplaces_collections table

--- sql/2_tables_chains_data.sql
+++ sql/2_tables_chains_data.sql
@@ -33,20 +33,29 @@ CREATE TABLE IF NOT EXISTS chains_marketplaces
 
 CREATE TABLE IF NOT EXISTS collections
 (
-    id          UUID PRIMARY KEY,
-    chain_id    UUID         NOT NULL REFERENCES chains ON DELETE CASCADE,
-    token_id    VARCHAR(128) NOT NULL,
-    address     VARCHAR(128) NOT NULL,
-    name        TEXT         NOT NULL,
-    description TEXT         NOT NULL,
-    symbol      VARCHAR(16)  NOT NULL,
-    slug        VARCHAR(256) NOT NULL,
-    standard    VARCHAR(128) NOT NULL,
-    uri         TEXT         NOT NULL,
-    website     TEXT         NOT NULL,
-    image_url   TEXT         NOT NULL,
-    created_at  TIMESTAMP DEFAULT NOW(),
-    updated_at  TIMESTAMP
+    id                     UUID PRIMARY KEY,
+    chain_id               UUID         NOT NULL REFERENCES chains ON DELETE CASCADE,
+    contract_collection_id VARCHAR(128) NOT NULL,
+    address                VARCHAR(128) NOT NULL,
+    name                   TEXT         NOT NULL,
+    description            TEXT         NOT NULL,
+    symbol                 VARCHAR(16)  NOT NULL,
+    slug                   VARCHAR(256) NOT NULL,
+    standard               VARCHAR(128) NOT NULL,
+    uri                    TEXT         NOT NULL,
+    website                TEXT         NOT NULL,
+    image_url              TEXT         NOT NULL,
+    created_at             TIMESTAMP DEFAULT NOW(),
+    updated_at             TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS marketplaces_collections
+(
+    marketplace_id UUID NOT NULL REFERENCES marketplaces ON DELETE CASCADE,
+    collection_id  UUID NOT NULL REFERENCES collections ON DELETE CASCADE,
+    created_at     TIMESTAMP DEFAULT NOW(),
+    updated_at     TIMESTAMP,
+    PRIMARY KEY (marketplace_id, collection_id)
 );
 
 CREATE TABLE IF NOT EXISTS nfts
