23531735d2b88cee2764a9356f730ce8417a9f04
Max Wolter <awfm9@protonmail.com>
2022-04-23 13:17:57 +0100

    Rebase

--- sql/3_insert_collections.sql
+++ sql/3_insert_collections.sql
@@ -5,8 +5,7 @@ INSERT INTO collections (id, chain_id, contract_collection_id, address, name, de
 VALUES ('abc44f5b-e4c7-46c2-9b2f-629c5bd763a6', '94c754fe-e06c-4d2b-bb76-2faa240b5bb8', NULL,
         '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d', 'Bored Ape Yacht Club',
         'The Bored Ape Yacht Club is a collection of 10,000 unique Bored Ape NFTs— unique digital collectibles living on the Ethereum blockchain. Your Bored Ape doubles as your Yacht Club membership card, and grants access to members-only benefits, the first of which is access to THE BATHROOM, a collaborative graffiti board. Future areas and perks can be unlocked by the community through roadmap activation. Visit www.BoredApeYachtClub.com for more details.',
-        'BAYC', 'boredapeyachtclub', 'https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/{{ .nft_id }}',
-        'https://www.boredapeyachtclub.com', '');
+        'BAYC', 'boredapeyachtclub', 'https://www.boredapeyachtclub.com', '');
 
 
 INSERT INTO standards (id, name)
