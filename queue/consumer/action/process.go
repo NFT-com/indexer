59e888a67b6a5b9d38928d1274b60a5c82d53772
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-04-25 15:25:09 +0100

    replaced the addition pipeline for an action pipeline

--- queue/consumer/action/process.go
+++ queue/consumer/action/process.go
@@ -3,10 +3,11 @@ package action
 import (
 	"fmt"
 
+	"github.com/NFT-com/indexer/action"
 	"github.com/NFT-com/indexer/models/chain"
 )
 
-func (d *Action) processNFT(nft chain.NFT) error {
+func (d *Action) processNFT(actionType string, nft chain.NFT) error {
 	chain, err := d.dataStore.Chain(nft.ChainID)
 	if err != nil {
 		return fmt.Errorf("could not get chain: %w", err)
@@ -17,15 +18,23 @@ func (d *Action) processNFT(nft chain.NFT) error {
 		return fmt.Errorf("could not get collection: %w", err)
 	}
 
-	err = d.dataStore.UpsertNFT(nft, collection.ID)
-	if err != nil {
-		return fmt.Errorf("could not store nft: %w", err)
-	}
-
-	for _, trait := range nft.Traits {
-		err = d.dataStore.UpsertTrait(trait)
+	switch actionType {
+	case action.Addition:
+		err = d.dataStore.UpsertNFT(nft, collection.ID)
 		if err != nil {
-			return fmt.Errorf("could not store trait: %w", err)
+			return fmt.Errorf("could not store nft: %w", err)
+		}
+
+		for _, trait := range nft.Traits {
+			err = d.dataStore.UpsertTrait(trait)
+			if err != nil {
+				return fmt.Errorf("could not store trait: %w", err)
+			}
+		}
+	case action.OwnerChange:
+		err = d.dataStore.UpdateNFTOwner(collection.ID, nft.ID, nft.Owner)
+		if err != nil {
+			return fmt.Errorf("could not update nft owner (nft %s): %w", nft.ID, err)
 		}
 	}
 
--- queue/consumer/action/store.go
+++ queue/consumer/action/store.go
@@ -8,6 +8,8 @@ type Store interface {
 	UpsertNFT(nft chain.NFT, collectionID string) error
 	UpsertTrait(trait chain.Trait) error
 
+	UpdateNFTOwner(collectionID, nft, owner string) error
+
 	Chain(chainID string) (*chain.Chain, error)
 	Collection(chainID, address, contractCollectionID string) (*chain.Collection, error)
 }
