6968d34d881e348ec2e39d4a58ea3ffceefcc8f7
Maelkum <pathlidge.ann+github@gmail.com>
2022-05-13 07:28:59 +0200

    Add collections - Fighter, Otherdeed, Something (Beanz)

--- sql/07_insert_collections_data.sql
+++ sql/07_insert_collections_data.sql
@@ -13,11 +13,51 @@ VALUES
     'boredapeyachtclub',
     'http://www.boredapeyachtclub.com/',
     'https://ik.imagekit.io/bayc/assets/bayc-logo.png'
+  ),
+  (
+    '42321e81-38a8-4a3d-8ee9-4a9b4ad68d1b',
+    '94c754fe-e06c-4d2b-bb76-2faa240b5bb8',
+    '0x34d85c9CDeB23FA97cb08333b511ac86E1C4E258',
+    14672945,
+    'Otherdeed',
+    'Otherdeed is the key to claiming land in Otherside. Each have a unique blend of environment and sediment — some with resources, some home to powerful artifacts. And on a very few, a Koda roams.',
+    'OTHR',
+    'otherdeed',
+    'https://otherside.xyz/',
+    ''
+  ),
+  (
+    '37f5eff7-e355-4d8b-9a35-8bfa4f819fef',
+    '94c754fe-e06c-4d2b-bb76-2faa240b5bb8',
+    '0x87E738a3d5E5345d6212D8982205A564289e6324',
+    14113471,
+    'Fighter',
+    'The on-chain idle MMO.',
+    'FIGHTER',
+    'fighter',
+    'https://raid.party/',
+    ''
+  ),
+  (
+    'c34f1bd8-c0d9-47d8-b4a4-6447a019a9cd',
+    '94c754fe-e06c-4d2b-bb76-2faa240b5bb8',
+    '0x306b1ea3ecdf94aB739F1910bbda052Ed4A9f949',
+    14492070,
+    'Something',
+    'BEANZ coming soon.',
+    'SMTH',
+    'something',
+    '',
+    ''
   );
+
 
 
 -- ERC-721 contracts scraped from Etherscan.io
 INSERT INTO collections_standards
   (collection_id, standard_id)
 VALUES
-  ('612ecc22-36ef-4ef7-bb0b-5b864b85d089', 'f7d4c503-3a75-49c8-b72b-e18b30e14d6a');
+  ('612ecc22-36ef-4ef7-bb0b-5b864b85d089', 'f7d4c503-3a75-49c8-b72b-e18b30e14d6a'),
+  ('42321e81-38a8-4a3d-8ee9-4a9b4ad68d1b', 'f7d4c503-3a75-49c8-b72b-e18b30e14d6a'),
+  ('37f5eff7-e355-4d8b-9a35-8bfa4f819fef', 'f7d4c503-3a75-49c8-b72b-e18b30e14d6a'),
+  ('c34f1bd8-c0d9-47d8-b4a4-6447a019a9cd', 'f7d4c503-3a75-49c8-b72b-e18b30e14d6a');
