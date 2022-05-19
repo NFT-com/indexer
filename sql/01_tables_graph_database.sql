64ea82253c98166a635092ea1fb558986d1d96a6
Ricardo Gonçalves <43743785+ricardomgoncalves@users.noreply.github.com>
2022-05-19 11:17:46 +0100

    Create jobs for marketplaces parsing (#130)
    Co-authored-by: Brendan Le Glaunec <harold.kutenberg@gmail.com>

--- sql/01_tables_graph_database.sql
+++ sql/01_tables_graph_database.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS networks
 (
     id          UUID PRIMARY KEY,
-    chain_id    NUMERIC      NOT NULL,
-    name        TEXT         NOT NULL,
-    description TEXT         NOT NULL,
-    symbol      VARCHAR(16)  NOT NULL
+    chain_id    NUMERIC     NOT NULL,
+    name        TEXT        NOT NULL,
+    description TEXT        NOT NULL,
+    symbol      VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS marketplaces
@@ -17,60 +17,68 @@ CREATE TABLE IF NOT EXISTS marketplaces
 
 CREATE TABLE IF NOT EXISTS collections
 (
-    id                  UUID PRIMARY KEY,
-    network_id          UUID         NOT NULL REFERENCES networks ON DELETE CASCADE,
-    contract_address    VARCHAR(128) NOT NULL,
-    start_height        NUMERIC      NOT NULL,
-    name                TEXT         NOT NULL,
-    description         TEXT         NOT NULL,
-    symbol              VARCHAR(16)  NOT NULL,
-    slug                VARCHAR(256) NOT NULL,
-    website             TEXT         NOT NULL,
-    image_url           TEXT         NOT NULL
+    id               UUID PRIMARY KEY,
+    network_id       UUID         NOT NULL REFERENCES networks ON DELETE CASCADE,
+    contract_address VARCHAR(128) NOT NULL,
+    start_height     NUMERIC      NOT NULL,
+    name             TEXT         NOT NULL,
+    description      TEXT         NOT NULL,
+    symbol           VARCHAR(16)  NOT NULL,
+    slug             VARCHAR(256) NOT NULL,
+    website          TEXT         NOT NULL,
+    image_url        TEXT         NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS nfts
 (
-    id             UUID         PRIMARY KEY,
-    collection_id  UUID         NOT NULL REFERENCES collections ON DELETE CASCADE,
-    token_id       VARCHAR(128) NOT NULL,
-    name           TEXT         NOT NULL,
-    uri            TEXT         NOT NULL,
-    image          TEXT         NULL,
-    description    TEXT         NULL,
-    owner          VARCHAR(128) NOT NULL,
-    created_at     TIMESTAMP DEFAULT NOW(),
-    updated_at     TIMESTAMP
+    id            UUID PRIMARY KEY,
+    collection_id UUID         NOT NULL REFERENCES collections ON DELETE CASCADE,
+    token_id      VARCHAR(128) NOT NULL,
+    name          TEXT         NOT NULL,
+    uri           TEXT         NOT NULL,
+    image         TEXT         NULL,
+    description   TEXT         NULL,
+    owner         VARCHAR(128) NOT NULL,
+    created_at    TIMESTAMP DEFAULT NOW(),
+    updated_at    TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS traits
 (
-    id         UUID PRIMARY KEY,
-    nft_id     UUID NOT NULL REFERENCES nfts ON DELETE CASCADE,
-    name       TEXT NOT NULL,
-    type       TEXT NOT NULL,
-    value      TEXT NOT NULL
+    id     UUID PRIMARY KEY,
+    nft_id UUID NOT NULL REFERENCES nfts ON DELETE CASCADE,
+    name   TEXT NOT NULL,
+    type   TEXT NOT NULL,
+    value  TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS standards
 (
-    id         UUID PRIMARY KEY,
-    name       TEXT NOT NULL
+    id   UUID PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS events
 (
-    id         UUID         PRIMARY KEY,
+    id         UUID PRIMARY KEY,
     event_hash VARCHAR(128) NOT NULL,
     name       TEXT         NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS networks_marketplaces
 (
-    network_id          UUID         NOT NULL REFERENCES networks ON DELETE CASCADE,
-    marketplace_id      UUID         NOT NULL REFERENCES marketplaces ON DELETE CASCADE,
-    contract_address    VARCHAR(128) NOT NULL,
+    network_id       UUID         NOT NULL REFERENCES networks ON DELETE CASCADE,
+    marketplace_id   UUID         NOT NULL REFERENCES marketplaces ON DELETE CASCADE,
+    contract_address VARCHAR(128) NOT NULL,
+    start_height     NUMERIC      NOT NULL,
     PRIMARY KEY (network_id, marketplace_id)
+);
+
+CREATE TABLE IF NOT EXISTS marketplaces_standards
+(
+    marketplace_id UUID NOT NULL REFERENCES marketplaces ON DELETE CASCADE,
+    standard_id    UUID NOT NULL REFERENCES standards ON DELETE CASCADE,
+    PRIMARY KEY (marketplace_id, standard_id)
 );
 
 CREATE TABLE IF NOT EXISTS marketplaces_collections
@@ -82,14 +90,14 @@ CREATE TABLE IF NOT EXISTS marketplaces_collections
 
 CREATE TABLE IF NOT EXISTS collections_standards
 (
-    collection_id   UUID NOT NULL REFERENCES collections ON DELETE CASCADE,
-    standard_id     UUID NOT NULL REFERENCES standards ON DELETE CASCADE,
-    PRIMARY KEY(collection_id, standard_id)
+    collection_id UUID NOT NULL REFERENCES collections ON DELETE CASCADE,
+    standard_id   UUID NOT NULL REFERENCES standards ON DELETE CASCADE,
+    PRIMARY KEY (collection_id, standard_id)
 );
 
 CREATE TABLE IF NOT EXISTS standards_events
 (
-    standard_id UUID    NOT NULL REFERENCES standards ON DELETE CASCADE,
-    event_id    UUID    NOT NULL REFERENCES events ON DELETE CASCADE,
-    PRIMARY KEY(standard_id, event_id)
+    standard_id UUID NOT NULL REFERENCES standards ON DELETE CASCADE,
+    event_id    UUID NOT NULL REFERENCES events ON DELETE CASCADE,
+    PRIMARY KEY (standard_id, event_id)
 );
