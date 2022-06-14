7ba8a17b80eddf6f89758fadc8e8e03c2e035044
Max Wolter <awfm9@protonmail.com>
2022-06-14 18:50:13 +0100

    Remove jobs from SQL schema

--- sql/03_tables_jobs_database.sql
+++ sql/03_tables_jobs_database.sql
@@ -1,49 +1,51 @@
+CREATE TABLE IF NOT EXISTS latest
+(
+    chain_id           NUMERIC        NOT NULL,
+    contract_address   VARCHAR(128)   NOT NULL,
+    event_hash         VARCHAR(256)   NOT NULL,
+    block_height       NUMERIC        NOT NULL,
+    updated_at         TIMESTAMP      NOT NULL
+)
+
 CREATE TABLE IF NOT EXISTS parsings
 (
-    id                 UUID PRIMARY KEY,
-    chain_id           NUMERIC        NOT NULL,
-    contract_addresses VARCHAR(128)[] NOT NULL,
-    event_hashes       VARCHAR(256)[] NOT NULL,
-    start_height       NUMERIC        NOT NULL,
-    end_height         NUMERIC        NOT NULL,
-    job_status         VARCHAR(64)    NOT NULL,
-    input_data         BYTEA          NOT NULL,
-    status_message     TEXT,
-    created_at         TIMESTAMP DEFAULT NOW(),
-    updated_at         TIMESTAMP
-);
+    id                  UUID PRIMARY KEY,
+    chain_id            NUMERIC         NOT NULL,
+    contract_addresses  VARCHAR(128)[]  NOT NULL,
+    event_hashes        VARCHAR(256)[]  NOT NULL,
+    start_height        NUMERIC         NOT NULL,
+    end_height          NUMERIC         NOT NULL,
+    input_data          BYTEA           NOT NULL,
+    failure_message     TEXT            NOT NULL,
+    created_at          TIMESTAMP       NOT NULL,
+    failed_at           TIMESTAMP       NOT NULL
+)
 
-CREATE INDEX parsings_chain_id_idx ON parsings(chain_id);
-
-CREATE INDEX parsings_contract_addresses_idx ON parsings(contract_addresses);
-
-CREATE INDEX parsings_event_hashes_idx ON parsings(event_hashes);
-
-CREATE INDEX parsings_job_status_idx ON parsings(job_status);
-
-CREATE INDEX parsings_start_height_idx ON parsings(start_height);
-
-CREATE INDEX parsings_end_height_idx ON parsings(end_height);
-
-CREATE TABLE IF NOT EXISTS actions
+CREATE TABLE IF NOT EXISTS additions
 (
-    id               UUID PRIMARY KEY,
-    chain_id         NUMERIC      NOT NULL,
-    contract_address VARCHAR(128) NOT NULL,
-    token_id         VARCHAR(256) NOT NULL,
-    action_type      VARCHAR(256) NOT NULL,
-    block_height     NUMERIC      NOT NULL,
-    job_status       VARCHAR(64)  NOT NULL,
-    input_data       BYTEA        NOT NULL,
-    status_message   TEXT,
-    created_at       TIMESTAMP DEFAULT NOW(),
-    updated_at       TIMESTAMP
-);
+    id                  UUID PRIMARY KEY,
+    chain_id            NUMERIC         NOT NULL,
+    contract_address    VARCHAR(128)    NOT NULL,
+    token_id            VARCHAR(128)    NOT NULL,
+    block_height        NUMERIC         NOT NULL,
+    new_owner           VARCHAR(128)    NOT NULL,
+    token_count         NUMERIC         NOT NULL,
+    failure_message     TEXT            NOT NULL,
+    created_at          TIMESTAMP       NOT NULL,
+    failed_at           TIMESTAMP       NOT NULL
+)
 
-CREATE INDEX actions_chain_id_idx ON actions(chain_id);
-
-CREATE INDEX actions_contract_address_idx ON actions(contract_address);
-
-CREATE INDEX actions_job_status_idx ON actions(job_status);
-
-CREATE INDEX actions_block_height_idx ON actions(block_height);
+CREATE TABLE IF NOT EXISTS moves
+(
+    id                  UUID PRIMARY KEY,
+    chain_id            NUMERIC         NOT NULL,
+    contract_address    VARCHAR(128)    NOT NULL,
+    token_id            VARCHAR(128)    NOT NULL,
+    block_height        NUMERIC         NOT NULL,
+    from_owner          VARCHAR(128)    NOT NULL,
+    to_owner            VARCHAR(128)    NOT NULL,
+    token_count         NUMERIC         NOT NULL,
+    failure_message     TEXT            NOT NULL,
+    created_at          TIMESTAMP       NOT NULL,
+    failed_at           TIMESTAMP       NOT NULL
+)
