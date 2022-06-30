bbb65056c51622010e6926fe4e1d61c2ebfe31b0
Max Wolter <awfm9@protonmail.com>
2022-06-30 23:42:04 +0100

    Rename parameter for event ID

--- models/id/event.go
+++ models/id/event.go
@@ -7,8 +7,8 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func Event(hash string, index uint) string {
-	eventHash := sha3.Sum256([]byte(fmt.Sprintf("%s-%d", hash, index)))
+func Event(tx string, index uint) string {
+	eventHash := sha3.Sum256([]byte(fmt.Sprintf("%s-%d", tx, index)))
 	eventID := uuid.Must(uuid.FromBytes(eventHash[:16]))
 	return eventID.String()
 }
