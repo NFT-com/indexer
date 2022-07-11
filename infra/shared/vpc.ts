2da4ec3c30fe7e35418a70f1186247baa47a889d
Joey <joeypunzel@gmail.com>
2022-07-11 13:03:27 -0500

    Fix/infra vpc (#175)
    * fix: customize vpc cidr for addtl envs
    
    * fix: revert subnet name chg
    
    * fix: cleanup
    
    Co-authored-by: Joey Punzel <joey@immutableholdings.com>

--- infra/shared/vpc.ts
+++ infra/shared/vpc.ts
@@ -4,9 +4,9 @@ import { getStage } from '../helper'
 export const createVPC = (): ec2.Vpc => {
   const stage = getStage()
   return new ec2.Vpc('vpc', {
-    cidrBlock: '10.1.0.0/16',
+    cidrBlock: process.env.VPC_CIDR,
     numberOfAvailabilityZones: 3,
-    numberOfNatGateways: 1,
+    numberOfNatGateways: 0,
     subnets: [
       { type: 'public', name: stage },
       { type: 'private', name: stage },
