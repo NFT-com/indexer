ed4d6fefe55305228f606338beb8331579d0ebcb
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2022-07-21 15:24:56 +0100

    fix hash checking logic

--- service/workers/completion_handler.go
+++ service/workers/completion_handler.go
@@ -187,7 +187,7 @@ func (p *CompletionHandler) Handle(ctx context.Context, completion *jobs.Complet
 		sale.CurrencyAddress = coinTransfer.CollectionAddress
 
 		// ... and get the nft transfers for each sale according to its transaction hash.
-		nftTransfers, ok := nftTransferLookup[sale.TransactionHash]
+		nftTransfers, ok := nftTransferLookup[sale.Hash()]
 		if !ok {
 			p.log.Warn().
 				Str("sale_id", sale.ID).
