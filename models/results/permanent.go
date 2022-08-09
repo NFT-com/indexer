566a3d2fae64e7b7a4c6e7b3a5eb32449177315c
Max Wolter <awfm9@protonmail.com>
2022-08-09 07:58:01 +0100

    Add slice errors to permanent ones

--- models/results/permanent.go
+++ models/results/permanent.go
@@ -31,6 +31,12 @@ func Permanent(err error) bool {
 	case strings.Contains(msg, "the message response is too large"):
 		return true
 
+	// runtime errors that should never happen...
+	case strings.Contains(msg, "index out of range"):
+		return true
+	case strings.Contains(msg, "slice bounds out of range"):
+		return true
+
 	default:
 		return false
 	}
