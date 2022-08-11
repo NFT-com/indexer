c6c415740ded363a741fa128434f39230f5820f1
Max Wolter <awfm9@protonmail.com>
2022-08-11 17:25:02 +0100

    Disable retrying for topic mismatch errors

--- models/results/permanent.go
+++ models/results/permanent.go
@@ -21,6 +21,10 @@ func Permanent(err error) bool {
 	case strings.Contains(msg, "the message response is too large"):
 		return true
 
+	// error when parsing event that should be fixed
+	case strings.Contains(msg, "invalid number of topics"):
+		return true
+
 	// runtime errors are bugs and should always hard fail
 	case strings.Contains(msg, "runtime error"):
 		return true
