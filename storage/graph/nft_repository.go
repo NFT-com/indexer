7b8cfe246151a9b78b3fd8db6fc40d413062683c
Max Wolter <awfm9@protonmail.com>
2022-08-23 19:37:00 +0100

    Add delete functionality to NFT repo

--- storage/graph/nft_repository.go
+++ storage/graph/nft_repository.go
@@ -111,3 +111,55 @@ func (n *NFTRepository) Upsert(nft *graph.NFT) error {
 
 	return nil
 }
+
+func (n *NFTRepository) Delete(dummies ...*graph.NFT) error {
+
+	if len(dummies) == 0 {
+		return nil
+	}
+
+	set := make(map[string]*graph.NFT, len(dummies))
+	for _, dummy := range dummies {
+		set[dummy.ID] = dummy
+	}
+
+	dummies = make([]*graph.NFT, 0, len(set))
+	for _, dummy := range set {
+		dummies = append(dummies, dummy)
+	}
+
+	query := n.build.
+		Insert("nfts").
+		Columns(
+			"id",
+			"collection_id",
+			"token_id",
+			"name",
+			"uri",
+			"image",
+			"description",
+			"deleted_at",
+		).
+		Suffix("ON CONFLICT (id) DO UPDATE SET " +
+			"deleted = TRUE" +
+			"deleted_at = EXCLUDED.deleted_at")
+
+	for _, dummy := range dummies {
+		query = query.Values(
+			dummy.ID,
+			dummy.CollectionID,
+			dummy.TokenID,
+			"",
+			"",
+			"",
+			"",
+			"NOW()",
+		)
+	}
+
+	_, err := query.Exec()
+	if err != nil {
+		return fmt.Errorf("could not execute query: %w", err)
+	}
+	return nil
+}
