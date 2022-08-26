c32626d0320f9f961e9f1fb5657e944a7657332c
Max Wolter <awfm9@protonmail.com>
2022-08-26 11:58:24 +0200

    Disambiguate deleted field for on conflict clause

--- storage/graph/nft_repository.go
+++ storage/graph/nft_repository.go
@@ -144,7 +144,7 @@ func (n *NFTRepository) Delete(deletions ...*graph.NFT) error {
 		Suffix("ON CONFLICT (id) DO UPDATE SET " +
 			"deleted = TRUE, " +
 			"deleted_at = EXCLUDED.deleted_at " +
-			"WHERE deleted = FALSE")
+			"WHERE nfts.deleted = FALSE")
 
 	for _, deletion := range deletions {
 		query = query.Values(
