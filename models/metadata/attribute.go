7254754b821f756e4a33fa8c8577de9413bd96ad
Max Wolter <awfm9@protonmail.com>
2022-08-29 19:55:20 +0100

    Support alternative trait value key (#222)
    * Support alternative trait value key
    
    * Change precedence of trait value keys

--- models/metadata/attribute.go
+++ models/metadata/attribute.go
@@ -4,4 +4,5 @@ type Attribute struct {
 	DisplayType string      `json:"display_type"`
 	TraitType   string      `json:"trait_type"`
 	Value       interface{} `json:"value"`
+	TraitValue  interface{} `json:"trait_value"`
 }
