cc6affe8085db060e0e5f8573b2b6112e323cdb1
Max Wolter <awfm9@protonmail.com>
2022-08-31 11:21:52 +0200

    Complete metadata token struct

--- models/metadata/token.go
+++ models/metadata/token.go
@@ -1,8 +1,13 @@
 package metadata
 
 type Token struct {
-	Name        string      `json:"name"`
-	Description string      `json:"description"`
-	Image       string      `json:"image"`
-	Attributes  []Attribute `json:"attributes"`
+	Image           string      `json:"image"`
+	ImageData       string      `json:"image_data"`
+	ExternalURL     string      `json:"external_url"`
+	Description     string      `json:"description"`
+	Name            string      `json:"name"`
+	BackgroundColor string      `json:"background_color"`
+	AnimationURL    string      `json:"animation_url"`
+	YoutubeURL      string      `json:"youtube_url"`
+	Attributes      []Attribute `json:"attributes"`
 }
